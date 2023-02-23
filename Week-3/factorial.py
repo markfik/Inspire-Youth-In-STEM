1881450b6ea2e17a7eb1020eabec91e62d62e145
markfik <125241003+markfik@users.noreply.github.com>
2023-02-23 11:08:26 +0300

    Add files via upload

--- Week-3/factorial.py
+++ Week-3/factorial.py
@@ -1,15 +1,15 @@
 
 def factorial(n):
     for i in range(0,n):
-        m = reversed(i)
-        fact_n = (n*(n-m))
+        fact_n = (n*(n-i))
         print(fact_n)
 
 factorial(4)
-'''
+
 #write a program to calculate the simple interest
-p = input("enter the principal amount:")
-r =input("enter the rate:")
-t =input("enter the time duration:")
-simple_interest = int(p)*int(r)*int(t)
-print("The simple interest is",simple_interest)'''
+def simple_interest(p,r,t):
+    simple_interest =(p*(r/100)*t)
+    print(simple_interest)
+
+simple_interest(2000,12,5)
+
