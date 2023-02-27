ed1e4e2e6de905b076dfaa0c38d59df578701906
markfik <125241003+markfik@users.noreply.github.com>
2023-02-27 11:28:31 +0300

    Add files via upload

--- Week-3/assignment.py
+++ Week-3/assignment.py
@@ -1,23 +1,14 @@
 favouriteMusicians = []
-#for musicians add favouriteMusicians
-#copy to a new list called celebs
-#sort the list
-#pop out two celebs
-#count the remaining celebs
-favouriteMusicians.insert(0,'kahush')
-favouriteMusicians.insert(1,'nyashinski')
-favouriteMusicians.insert(2,'21 savage')
-favouriteMusicians.insert(3,'quavo')
-favouriteMusicians.insert(4,'nf')
-favouriteMusicians.insert(5,'lowki')
-favouriteMusicians.insert(6,'davaji')
-favouriteMusicians.insert(7,'Don Toliver')
+celebs =['kahush','nyashinski','21 savage','quavo','lowki','davaji','Don Toliver']
+for celeb in celebs:
+    favouriteMusicians.append(celeb.title())
 print(favouriteMusicians)
-celebs =favouriteMusicians.copy()
+print(len(favouriteMusicians))
 celebs.sort()
 print(celebs)
+print(celebs)
+celebs.pop(-1)
 celebs.pop(-1)
 print(celebs)
-celebs.pop(-2)
-print(celebs)
 print(len(celebs))
+print(int(len(favouriteMusicians))- 2 == int(len(celebs)))
--- Week-3/factorial.py
+++ Week-3/factorial.py
@@ -1,11 +1,12 @@
+'''
+def factorial(i):
+    for x in range(1,i+1):
+        i *=x
+        print(i)
 
-def factorial(n):
-    for i in range(0,n):
-        fact_n = (n*(n-i))
-        print(fact_n)
+factorial(int(input("enter the number:")))
 
-factorial(4)
-
+factorial(input("enter the number"))
 #write a program to calculate the simple interest
 def simple_interest(p,r,t):
     simple_interest =(p*(r/100)*t)
@@ -13,3 +14,46 @@ def simple_interest(p,r,t):
 
 simple_interest(2000,12,5)
 
+#factorial of (n)
+def factorial(n):
+    if n < 0:
+        print("No factorial for negative numbers")
+    else:
+        return n * factorial(n-1)
+        
+num = int(input("Enter a number: "))
+print("Factorial of", num, "is", factorial(num))
+'''
+'''
+start_num = 1200
+count_by = 4
+end_num = 144
+break_num = start_num
+while break_num<end_num:
+    result =break_num +count_by
+
+else:
+    result =("Oops! Looks like your start value is greater than the end value. Please try again.")
+
+print(result)
+'''
+'''
+# HINT: modify the headlines list to verify your loop works with different inputs
+headlines = ["Local Bear Eaten by Man",
+             "Legislature Announces New Laws",
+             "Peasant Discovers Violence Inherent in System",
+             "Cat Rescues Fireman Stuck in Tree",
+             "Brave Knight Runs Away",
+             "Papperbok Review: Totally Triffic"]
+
+news_ticker = ""
+# write your loop here
+index = 0
+for headline in headlines:
+        news_ticker =headline[index] + " "
+        if len(news_ticker) >= 140:
+                break
+        print("the headline is 140 characters")
+        
+        elif(len(news_ticker) + len(headline[index] ))
+'''
